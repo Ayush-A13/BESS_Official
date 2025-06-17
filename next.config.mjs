cf16c770e30eaecbff4779dd11a7180844c428eb
Ayush Arora <z5478656@ad.unsw.edu.au>
2025-06-17 18:33:10 +1000

    v2.4 updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',          // Enable static export
-  images: {
-    unoptimized: true        // Required if you're using <Image> component without optimization on static hosting
-  }
+  output: 'export'
 };
 
 export default nextConfig;
